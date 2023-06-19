ffc66301436b940b2dd40f60c893de5b47a7ba94
Geraldo Jose <lantanggeraldo@gmail.com>
2023-06-19 16:34:32 +0700

    migrate to new db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "sikeuangan"
+    host: "dummyhost",
+    user: "dummyuser",
+    password: "dummypassword",
+    database: "dummydatabase"
 });
 
 module.exports = dbPool.promise();
- 
